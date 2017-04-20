a0e9def1e35483a6fb779c98b6c04c5662b70fe3
KevinChen <kevin.chen1994@outlook.com>
2017-04-20 11:31:09 +0800

    11

---
+++ SeleniumYeeOfficeTestng/src/main/java/kevin/SeleniumYeeOfficeTestng/App.java
@@ -0,0 +1,13 @@
+package kevin.SeleniumYeeOfficeTestng;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
